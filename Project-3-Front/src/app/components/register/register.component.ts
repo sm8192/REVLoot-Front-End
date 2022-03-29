2f0fad3dbf1ffc4fdbe7ef1da3f18bdee96d1c97
William Fuentes <fuenteswilliam0326@gmail.com>
2022-03-29 09:19:03 -0700

    making reg form

---
+++ Project-3-Front/src/app/components/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+onSubmit(data: any){//does not do anything yet just put it here to get rid of errors in html-william
+
+}
+}
