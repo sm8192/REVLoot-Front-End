1baa5197b776c773c762f34af2892dbe979a8922
Angeudy Brisita <angeudybp@gmail.com>
2022-03-29 14:26:06 -0400

    Registration form added
    Moved the component for the registration form into the login folder.

--- Project-3-Front/src/app/app.module.ts
+++ Project-3-Front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/Login/register/register.component';
 
 @NgModule({
   declarations: [
