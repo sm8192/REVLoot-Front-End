4436daf91d9d44947a390d2ec09d9d943ce6cb66
Christian <cgonzalez233@yahoo.com>
2022-03-29 22:19:32 -0400

    Added search pipe and started product service

--- Project-3-Front/src/app/app.module.ts
+++ Project-3-Front/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPipe } from './pipes/search.pipe';
+import { SearchTestComponent } from './components/group2/search-test/search-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe,
+    SearchTestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Project-3-Front/src/app/pipes/search.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(item => {
+      return item.toLocaleLowerCase().includes(searchText);
+    });
+  }
+
+}
