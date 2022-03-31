6f928614efcf5ecb0330c3b57a8ef21b5c52a15e
Elliot Culbertson <elliot@gmail.com>
2022-03-31 11:07:24 -0400

    connected productList page to back-end

--- Project-3-Front/src/app/components/group2/product-list-page/product-list-page.component.ts
+++ Project-3-Front/src/app/components/group2/product-list-page/product-list-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list-page',
@@ -7,50 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListPageComponent implements OnInit {
 
-  product: any;
+  // product: any;
 
   products: any[] = [
-    {
-      "productId":1,
-      "productName":"title",
-      "price":"1.00",
-      "description": "lorem ipsem",
-      "productQty":0,
-      "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
-    },
-    {
-      "productId":1,
-      "productName":"title",
-      "price":"1.00",
-      "description": "lorem ipsem",
-      "productQty":5,
-      "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
-    },
-    {
-      "productId":1,
-      "productName":"title",
-      "price":"1.00",
-      "description": "lorem ipsem",
-      "productQty":5,
-      "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
-    },
-    {
-      "productId":1,
-      "productName":"title",
-      "price":"1.00",
-      "description": "lorem ipsem",
-      "productQty":5,
-      "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
-    },
+    // {
+    //   "productId":1,
+    //   "productName":"title",
+    //   "price":"1.00",
+    //   "description": "lorem ipsem",
+    //   "productQty":0,
+    //   "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
+    // },
+    // {
+    //   "productId":1,
+    //   "productName":"title",
+    //   "price":"1.00",
+    //   "description": "lorem ipsem",
+    //   "productQty":5,
+    //   "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
+    // },
+    // {
+    //   "productId":1,
+    //   "productName":"title",
+    //   "price":"1.00",
+    //   "description": "lorem ipsem",
+    //   "productQty":5,
+    //   "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
+    // },
+    // {
+    //   "productId":1,
+    //   "productName":"title",
+    //   "price":"1.00",
+    //   "description": "lorem ipsem",
+    //   "productQty":5,
+    //   "productImage":"https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png"
+    // },
   ]
 
-  constructor() { }
+  constructor(private route: Router, private data: ProductService) { }
 
   ngOnInit(): void {
+    this.data.getProducts().subscribe(data => this.products = data)
+    console.log(this.data);
   }
 
-  onClick(product: any): void {
-    this.product = product;
+  onClick(productId: any): void {
+    this.route.navigate(['productPage', productId])
   }
 
 }
