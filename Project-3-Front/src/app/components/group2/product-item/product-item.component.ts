d00348a6b1973c8439249771a284ca489ab0ca01
Shane Moorhead <sm8192@gmail.com>
2022-04-01 13:26:15 -0500

    id fix

--- Project-3-Front/src/app/components/group2/product-item/product-item.component.ts
+++ Project-3-Front/src/app/components/group2/product-item/product-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
+import { Item } from 'src/app/services/item';
 
 @Component({
   selector: 'app-product-item',
@@ -10,15 +12,15 @@ import { ProductService } from 'src/app/services/product.service';
 
 export class ProductItemComponent implements OnInit {
 
-  productItem: any; 
+  productItem: Item = {id: 0, price: 0, productName: "", description: "", productQty: 0, productImage: ""}
   id: any;
   orderedItems = 0;
   orderCount = 0;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService,private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get("productId")
+    this.id = this.route.snapshot.paramMap.get("id")
     this.productService.getProductById(this.id).subscribe(resp => { 
       this.productItem = resp;
       console.log(this.productItem)
@@ -27,6 +29,7 @@ export class ProductItemComponent implements OnInit {
 
   onFormSubmit(data: any){
     console.log(data)
+//    this.cartService.addItem(data)
     //Uncommit when change to new obj for cart
     // this.productService.addProduct(this.productItem).subscribe(resp => {
     //   this.router.navigateByUrl("product-list");
--- Project-3-Front/src/app/components/group2/product-list-page/product-list-page.component.ts
+++ Project-3-Front/src/app/components/group2/product-list-page/product-list-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Item } from 'src/app/services/item';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,148 +11,7 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListPageComponent implements OnInit {
 
 
-  products: any[] = 
-  [{
-    "id": 1,
-    "productName": "Toyota",
-    "price": 85.38,
-    "description": "Tacoma",
-    "productQty": 7,
-    "productImage": "http://dummyimage.com/161x100.png/cc0000/ffffff"
-  }, {
-    "id": 2,
-    "productName": "Ford",
-    "price": 86.37,
-    "description": "Econoline E150",
-    "productQty": 1,
-    "productImage": "http://dummyimage.com/123x100.png/cc0000/ffffff"
-  }, {
-    "id": 3,
-    "productName": "Ford",
-    "price": 55.15,
-    "description": "Explorer Sport Trac",
-    "productQty": 4,
-    "productImage": "http://dummyimage.com/199x100.png/dddddd/000000"
-  }, {
-    "id": 4,
-    "productName": "Suzuki",
-    "price": 55.65,
-    "description": "Grand Vitara",
-    "productQty": 7,
-    "productImage": "http://dummyimage.com/115x100.png/cc0000/ffffff"
-  }, {
-    "id": 5,
-    "productName": "Acura",
-    "price": 76.66,
-    "description": "NSX",
-    "productQty": 8,
-    "productImage": "http://dummyimage.com/199x100.png/ff4444/ffffff"
-  }, {
-    "id": 6,
-    "productName": "Saturn",
-    "price": 37.43,
-    "description": "S-Series",
-    "productQty": 10,
-    "productImage": "http://dummyimage.com/235x100.png/ff4444/ffffff"
-  }, {
-    "id": 7,
-    "productName": "Mercedes-Benz",
-    "price": 32.73,
-    "description": "S-Class",
-    "productQty": 10,
-    "productImage": "http://dummyimage.com/104x100.png/ff4444/ffffff"
-  }, {
-    "id": 8,
-    "productName": "Mercury",
-    "price": 84.98,
-    "description": "Cougar",
-    "productQty": 1,
-    "productImage": "http://dummyimage.com/235x100.png/dddddd/000000"
-  }, {
-    "id": 9,
-    "productName": "Maybach",
-    "price": 34.27,
-    "description": "57",
-    "productQty": 5,
-    "productImage": "http://dummyimage.com/148x100.png/cc0000/ffffff"
-  }, {
-    "id": 10,
-    "productName": "Chevrolet",
-    "price": 21.63,
-    "description": "Malibu",
-    "productQty": 1,
-    "productImage": "http://dummyimage.com/171x100.png/5fa2dd/ffffff"
-  }, {
-    "id": 11,
-    "productName": "Mitsubishi",
-    "price": 70.54,
-    "description": "Precis",
-    "productQty": 4,
-    "productImage": "http://dummyimage.com/128x100.png/5fa2dd/ffffff"
-  }, {
-    "id": 12,
-    "productName": "Ford",
-    "price": 65.22,
-    "description": "F150",
-    "productQty": 2,
-    "productImage": "http://dummyimage.com/109x100.png/cc0000/ffffff"
-  }, {
-    "id": 13,
-    "productName": "Dodge",
-    "price": 74.75,
-    "description": "D150",
-    "productQty": 7,
-    "productImage": "http://dummyimage.com/231x100.png/dddddd/000000"
-  }, {
-    "id": 14,
-    "productName": "Infiniti",
-    "price": 40.43,
-    "description": "IPL G",
-    "productQty": 7,
-    "productImage": "http://dummyimage.com/239x100.png/5fa2dd/ffffff"
-  }, {
-    "id": 15,
-    "productName": "Nissan",
-    "price": 92.78,
-    "description": "GT-R",
-    "productQty": 3,
-    "productImage": "http://dummyimage.com/219x100.png/5fa2dd/ffffff"
-  }, {
-    "id": 16,
-    "productName": "Mercedes-Benz",
-    "price": 24.89,
-    "description": "SL-Class",
-    "productQty": 2,
-    "productImage": "http://dummyimage.com/168x100.png/cc0000/ffffff"
-  }, {
-    "id": 17,
-    "productName": "Bentley",
-    "price": 29.28,
-    "description": "Azure T",
-    "productQty": 1,
-    "productImage": "http://dummyimage.com/190x100.png/5fa2dd/ffffff"
-  }, {
-    "id": 18,
-    "productName": "Ford",
-    "price": 49.48,
-    "description": "E-Series",
-    "productQty": 2,
-    "productImage": "http://dummyimage.com/215x100.png/ff4444/ffffff"
-  }, {
-    "id": 19,
-    "productName": "Acura",
-    "price": 59.07,
-    "description": "Integra",
-    "productQty": 2,
-    "productImage": "http://dummyimage.com/135x100.png/cc0000/ffffff"
-  }, {
-    "id": 20,
-    "productName": "Cadillac",
-    "price": 28.95,
-    "description": "Eldorado",
-    "productQty": 6,
-    "productImage": "http://dummyimage.com/152x100.png/5fa2dd/ffffff"
-  }]
+  products: Item[] = []
 
   constructor(private route: Router, private productService: ProductService) { }
 
@@ -159,7 +19,7 @@ export class ProductListPageComponent implements OnInit {
     this.productService.getProducts().subscribe(data => this.products = data)
   }
 
-  onClick(productId: any): void {
+  onClick(productId: number): void {
     console.log(productId)
     this.route.navigate(['productItem', productId])
   }
